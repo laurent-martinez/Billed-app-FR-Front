1a0e4e091c8b1451684457692da82f39cb2c82d0
laurent-martinez <martilaurier@gmail.com>
2022-11-29 15:54:53 +0100

    add expect on bill to

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -25,8 +25,8 @@ describe("Given I am connected as an employee", () => {
       window.onNavigate(ROUTES_PATH.Bills)
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
-      //to-do write expect expression
-
+      //check if dashboard icon is highlighted with the class 'active-icon
+      expect(windowIcon.className).toBe("active-icon")
     })
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
